c514b07ba75e2b9417cd3adc6b2933b6bf72ddd8
iCAMPAGNE <development@icampagne.nl>
2023-01-01 18:24:33 +0100

    Scaling with ships works.

--- frontend/src/app/components/mousewheel-scrolling/mousewheel-scrolling.component.ts
+++ frontend/src/app/components/mousewheel-scrolling/mousewheel-scrolling.component.ts
@@ -9,13 +9,15 @@ export class MousewheelScrollingComponent implements OnInit, AfterViewInit {
   @ViewChild('timeSpan') timeSpan: ElementRef<HTMLInputElement> | undefined;
 
   zoomFactor: number = 5;
-  clientWidth: number = 0;
+  clientMiddle: number = 0;
 
   marginLeft: number = 0;
   minutes: number[] = Array.from(Array(100).keys());
   minuteWidth: number = 25;
-  scrollLeft:number = 0;
+  scrollLeft: number = 0;
   printNr: number = 0;
+
+  ships = [{pos: {x: 80, y:0}, length: 2}, {pos: {x: 79, y:25}, length: 0.5}, {pos: {x: 77, y:0}, length: 1.5}, {pos: {x: 70, y:75}, length: 4}];
 
   ngOnInit(): void {
   }
@@ -23,20 +25,21 @@ export class MousewheelScrollingComponent implements OnInit, AfterViewInit {
   ngAfterViewInit(): void {
     if (this.timeSpan && this.timeSpan.nativeElement) {
       const timeSpanElement = this.timeSpan.nativeElement;
-      this.clientWidth = timeSpanElement.clientWidth / 2;
-      timeSpanElement.scrollLeft = 75 * this.minuteWidth - timeSpanElement.clientWidth / 2;
+      this.clientMiddle = timeSpanElement.clientWidth / 2;
       this.marginLeft = timeSpanElement.offsetLeft;
+      timeSpanElement.scrollLeft = 75 * this.minuteWidth - timeSpanElement.clientWidth / 2;
       this.printValues();
     }
   }
 
   @HostListener("wheel", ["$event"])
   public onScroll(event: WheelEvent) {
+    this.printValues(event);
     const currentMinuteWidth = this.minuteWidth;
     let factor = 0;
     if (this.timeSpan && this.timeSpan.nativeElement) {
-      const timeSpanEl = this.timeSpan.nativeElement;
-      this.scrollLeft = timeSpanEl.scrollLeft;
+      const timeSpanElement = this.timeSpan.nativeElement;
+      this.scrollLeft = timeSpanElement.scrollLeft;
 
       if (event.deltaY > 0 && this.minuteWidth > this.zoomFactor) {
         this.minuteWidth -= this.zoomFactor;
@@ -46,12 +49,14 @@ export class MousewheelScrollingComponent implements OnInit, AfterViewInit {
         this.minuteWidth += this.zoomFactor;
         factor = this.zoomFactor;
       }
-      timeSpanEl.scrollTo(timeSpanEl.scrollLeft + factor * (timeSpanEl.scrollLeft + (event.clientX - this.clientWidth - this.marginLeft) + timeSpanEl.clientWidth / 2) / currentMinuteWidth, 0);
+      timeSpanElement.scrollTo(timeSpanElement.scrollLeft + factor * (timeSpanElement.scrollLeft + (event.clientX - this.clientMiddle - this.marginLeft) + timeSpanElement.clientWidth / 2) / currentMinuteWidth, 0);
     }
     setTimeout(() => {
+      this.printValues(event);
       if (this.timeSpan && this.timeSpan.nativeElement) {
         const timeSpanElement = this.timeSpan.nativeElement;
-       timeSpanElement.scrollTo(this.scrollLeft + factor * (this.scrollLeft + (event.clientX - this.clientWidth - this.marginLeft) + timeSpanElement.clientWidth / 2) / currentMinuteWidth , 0);
+        timeSpanElement.scrollTo(this.scrollLeft + factor * (this.scrollLeft + (event.clientX - this.clientMiddle - this.marginLeft) + timeSpanElement.clientWidth / 2) / currentMinuteWidth , 0);
+//        this.ship.pos += factor * this.minuteWidth + 225;
 
         this.printValues(event);
       }
@@ -60,8 +65,8 @@ export class MousewheelScrollingComponent implements OnInit, AfterViewInit {
 
   printValues(event?: WheelEvent) {
     if (this.printNr++ % 5 == 0) {
-      console.log('     client\tclient\tscroll\tscroll\t');
-      console.log('zoom Left\tWidth\tLeft\tWidth\tmouse');
+      console.log('     client\tclient\tscroll\tscroll\t\tminute');
+      console.log('zoom Left\tWidth\tLeft\tWidth\tmouse\twidth');
     }
     if (this.timeSpan && this.timeSpan.nativeElement) {
       const timeSpanElement = this.timeSpan.nativeElement;
@@ -71,6 +76,7 @@ export class MousewheelScrollingComponent implements OnInit, AfterViewInit {
           + '\t' + timeSpanElement.scrollLeft
           + '\t' + timeSpanElement.scrollWidth
           + '\t' + (event ? event.clientX : '')
+          + '\t' + this.minuteWidth
           + '\t' + (75 * this.minuteWidth * 5 - timeSpanElement.clientWidth / 2)
           + '\t' + (timeSpanElement.scrollLeft + (event ? event.clientX - 258: 0) + timeSpanElement.clientWidth / 2) / (this.minuteWidth * 5)
           + '\t'
