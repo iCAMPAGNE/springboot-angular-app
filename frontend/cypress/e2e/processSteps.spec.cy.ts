62eaec3306b99122a5abf084ec5bd4312e595f04
iCAMPAGNE <development@icampagne.nl>
2024-04-08 19:23:17 +0200

    v0.2.20  Installed Cypress + first e2e test.

---
+++ frontend/cypress/e2e/processSteps.spec.cy.ts
@@ -0,0 +1,29 @@
+describe('Process steps spec', () => {
+  beforeEach(() => {
+    cy.visit('http://localhost:4212/springboot-angular-app/')
+    cy.contains('Process steps').click()
+    cy.url().should('include', '/processSteps')
+    cy.contains('Toon proces stappen').click()
+    cy.wait(8000)
+  })
+
+  it('navigate to process steps, create new step and show steps', () => {
+    // Get textarea, type into it
+    cy.get('textarea').type('This is the first step to be taken.')
+    //  Verify that the content has been updated
+    cy.get('textarea').should('have.value', 'This is the first step to be taken.')
+
+    cy.get('select').select('bovenaan')
+
+    cy.contains('Voeg proces stap toe').click();
+
+    cy.wait(2000)
+    cy.get('textarea').clear()
+    cy.get('textarea').type('This is the seconds step to be taken.')
+    cy.get('select').select('Na stap 1')
+    cy.contains('Voeg proces stap toe').click();
+
+    cy.wait(2000)
+    cy.contains('Toon proces stappen').click()
+  })
+})
