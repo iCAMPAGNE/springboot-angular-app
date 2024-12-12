fd5d62f740ebea63a017eb6d5e9a3f9c3e55f468
iCAMPAGNE <development@icampagne.nl>
2024-12-12 21:27:13 +0100

    v0.4.2  Finished 3-D basic, added window-popup type

--- frontend/src/app/components/home/home.component.ts
+++ frontend/src/app/components/home/home.component.ts
@@ -1,4 +1,4 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FirstService} from "../../services/first.service";
 
 @Component({
@@ -6,11 +6,15 @@ import {FirstService} from "../../services/first.service";
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, OnDestroy {
   demoButton1Disabled: boolean = false;
   demoButton2Disabled: boolean = false;
   processing: boolean = false;
   checkBoxValid: boolean = false;
+  contentType1: string = '';
+  contentType2: string = '';
+  contentType3: string = '';
+  win: Window | null = null;
 
   constructor(private firstService: FirstService) {
   }
@@ -35,5 +39,53 @@ export class HomeComponent implements OnInit {
       this.processing = false;
       this.demoButton2Disabled = false;
     }, 5000);
+  }
+
+  openXmlWindow() {
+    this.contentType1 = '';
+    this.contentType2 = '';
+    this.contentType3 = '';
+    const blob = new Blob(['<name>Frans</name>'], { type: 'text/xml' });
+    const fileURL = window.URL.createObjectURL(blob);
+    this.win = window.open(fileURL, 'MyWindow', 'popup=true, width=180,height=300,left=1840,top=50');
+    if (this.win != null) {
+      this.contentType1 = '' + this.win.document.contentType;
+      this.win.document.addEventListener('loaded',   () => {
+        this.contentType2 = '' + this.win?.document.contentType;
+      }, true);
+      setTimeout(() => {
+        this.contentType3 = '' + this.win?.document.contentType;
+      }, 1000);
+    }
+  }
+
+  openHtmlWindow() {
+    this.contentType1 = '';
+    this.contentType2 = '';
+    this.contentType3 = '';
+    const blob = new Blob(['<html><body><h2>HTML file</h2></body></html>'], { type: 'text/html' });
+    const fileURL = window.URL.createObjectURL(blob);
+    if (this.win != null) {
+      this.win.open(fileURL, 'MyWindow', 'popup=true, width=400,height=600,left=960,top=0');
+      this.contentType1 = this.win.document.contentType;
+      this.win.onload = () => {
+        this.contentType2 = this.win?.document.contentType + '';
+      }
+      setTimeout(() => {
+        this.contentType3 = this.win?.document.contentType + '';
+      }, 1000);
+    }
+  }
+
+  closeWindow() {
+    this.win?.close();
+    this.contentType1 = '';
+    this.contentType2 = '';
+    this.contentType3 = '';
+  }
+
+
+  ngOnDestroy(): void {
+    this.win?.close();
   }
 }
--- frontend/src/app/components/three-d/three-d.component.ts
+++ frontend/src/app/components/three-d/three-d.component.ts
@@ -1,17 +1,49 @@
-import { Component } from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 
 @Component({
   selector: 'app-three-d',
   templateUrl: './three-d.component.html',
   styleUrls: ['./three-d.component.scss']
 })
-export class ThreeDComponent {
+export class ThreeDComponent implements OnInit {
+  PPC = 50; // Pixels Per Centimeter
+  ScreenDistance = 12 * this.PPC;
+  PlanetRadius = 60;
+  PlanetOrbitRadius = 600;
+  ScreenToPlanetMiddleDistance = 300;
+  PlanetOrbitPositionX = 700;
+  PlanetOrbitPositionY = 400;
+
+  planetPositionX = this.PlanetOrbitPositionX + Math.sin(2*Math.PI/3) * this.PlanetOrbitRadius;
+  planetPositionY = this.PlanetOrbitPositionY;
+
+  objectOffset = 0;
 
   circleOffset:number = 0;
   circlePos:number = 0;
-  radius:number = 30;
+  radius:number = this.PlanetRadius * (Math.cos(2*Math.PI/3) * this.PlanetOrbitRadius + this.ScreenDistance + this.ScreenToPlanetMiddleDistance) / this.ScreenDistance;
+
 
   ngOnInit(): void {
+    this.planetOrbit();
+  }
+
+  planetOrbit(): void {
+    for (let t = 2*Math.PI/3; t < 14*Math.PI/3; t += 0.002*Math.PI) {
+      setTimeout(() => {
+        let planetDistance= Math.sqrt(
+          Math.pow(Math.cos(t) * this.PlanetOrbitRadius + this.ScreenDistance + this.ScreenToPlanetMiddleDistance,2) +
+          Math.pow(Math.sin(t) * this.PlanetOrbitRadius,2));
+        this.radius = this.PlanetRadius * this.ScreenDistance / planetDistance;
+        this.planetPositionX = this.PlanetOrbitPositionX + this.ScreenDistance * this.PlanetOrbitRadius*Math.sin(t) /(this.ScreenDistance + this.ScreenToPlanetMiddleDistance + this.PlanetOrbitRadius*Math.cos(t));
+        this.objectOffset = 2 * this.PPC * (Math.cos(t) * this.PlanetOrbitRadius + this.ScreenToPlanetMiddleDistance) / (Math.cos(t) * this.PlanetOrbitRadius + this.ScreenDistance + this.ScreenToPlanetMiddleDistance);
+
+      }, 5000*t);
+
+    }
+  }
+
+  firstMovement(): void {
     let t = 0;
     for(;t<250; t++) {
       setTimeout(() => {
