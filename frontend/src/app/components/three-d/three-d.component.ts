919754b810b4087700a6d35f1a9d886938e6e7ab
iCAMPAGNE <development@icampagne.nl>
2024-12-15 19:50:01 +0100

    3-D continued #4 Orbit also vertical.

--- frontend/src/app/components/three-d/three-d.component.ts
+++ frontend/src/app/components/three-d/three-d.component.ts
@@ -1,6 +1,8 @@
 import {Component, OnInit} from '@angular/core';
 
 const PPC = 50; // Pixels Per Centimeter
+const SCREEN_WIDTH = 1920;
+const SCREEN_HEIGHT = 1080;
 const EYE_DISTANCE = PPC * 6;
 const SCREEN_DISTANCE = PPC * 12; // Distance from viewer to screen.
 const ORBIT_CENTER = PPC * 5; // Distance from center of orbit to screen
@@ -19,8 +21,9 @@ export class ThreeDComponent implements OnInit {
 
   planetOrbit(): void {
     const startingAngle  = Math.PI - Math.acos(ORBIT_CENTER/(PPC * 12));
-    this.rotatingObjects.push(new RotatingObject(PPC * 1.2, PPC * 12, startingAngle));
-    this.rotatingObjects.push(new RotatingObject(PPC * 1.2, PPC * 9, 0));
+    this.rotatingObjects.push(new RotatingObject(PPC * 1.2, PPC * 12, 0, 1, startingAngle));
+    this.rotatingObjects.push(new RotatingObject(PPC * 1.2, PPC * 9, Math.PI/4, 1.5, 0));
+    this.rotatingObjects.push(new RotatingObject(PPC * 0.6, PPC * 7, Math.PI/2, 1.5, Math.PI));
     for (let t = 0; t <= 2000; t++) {
       setTimeout(() => {
         this.rotatingObjects.forEach(rotatingObject => {
@@ -29,33 +32,21 @@ export class ThreeDComponent implements OnInit {
       }, 20*t + 2000);
     }
   }
-
-  rotateObject(rotatingObject: RotatingObject, t: number) {
-    const planetDistance= Math.sqrt(
-      Math.pow(Math.cos(rotatingObject.getAngle(t)) * rotatingObject.orbitRadius + SCREEN_DISTANCE + ORBIT_CENTER,2) +
-      Math.pow(Math.sin(rotatingObject.getAngle(t)) * rotatingObject.orbitRadius,2));
-    rotatingObject.opticalRadius = rotatingObject.radius * SCREEN_DISTANCE / planetDistance;
-    rotatingObject.planetPositionX = PPC * 18 + SCREEN_DISTANCE * rotatingObject.orbitRadius*Math.sin(rotatingObject.getAngle(t)) /(SCREEN_DISTANCE + ORBIT_CENTER + rotatingObject.orbitRadius*Math.cos(rotatingObject.getAngle(t)));
-    rotatingObject.threeDoffset = (EYE_DISTANCE/2) * (Math.cos(rotatingObject.getAngle(t)) * rotatingObject.orbitRadius + ORBIT_CENTER) / (Math.cos(rotatingObject.getAngle(t)) * rotatingObject.orbitRadius + SCREEN_DISTANCE + ORBIT_CENTER);
-  }
 }
 
 class RotatingObject {
   opticalRadius = 0; // As seen by the viewer
   threeDoffset = 0;
-  planetPositionX!: number
+  opticalX!: number
+  opticalY!: number
   constructor(
     public radius: number,
     public orbitRadius: number,
+    public diskAngle: number,
+    public speed: number,
     public startingAngle: number
   ) {
     this.rotateObject(0);
-    // const planetDistance= Math.sqrt(
-    //   Math.pow(Math.cos(this.getAngle(startingAngle)) * this.orbitRadius + SCREEN_DISTANCE + ORBIT_CENTER,2) +
-    //   Math.pow(Math.sin(this.getAngle(startingAngle)) * this.orbitRadius,2));
-    // this.opticalRadius = this.radius * SCREEN_DISTANCE / planetDistance;
-    // this.planetPositionX = PPC * 18 + SCREEN_DISTANCE * this.orbitRadius*Math.sin(this.getAngle(startingAngle)) /(SCREEN_DISTANCE + ORBIT_CENTER + this.orbitRadius*Math.cos(this.getAngle(startingAngle)));
-    // this.threeDoffset = (EYE_DISTANCE/2) * (Math.cos(this.getAngle(startingAngle)) * this.orbitRadius + ORBIT_CENTER) / (Math.cos(this.getAngle(startingAngle)) * this.orbitRadius + SCREEN_DISTANCE + ORBIT_CENTER);
   }
 
   rotateObject(t: number) {
@@ -63,11 +54,13 @@ class RotatingObject {
       Math.pow(Math.cos(this.getAngle(t)) * this.orbitRadius + SCREEN_DISTANCE + ORBIT_CENTER,2) +
       Math.pow(Math.sin(this.getAngle(t)) * this.orbitRadius,2));
     this.opticalRadius = this.radius * SCREEN_DISTANCE / planetDistance;
-    this.planetPositionX = PPC * 18 + SCREEN_DISTANCE * this.orbitRadius*Math.sin(this.getAngle(t)) /(SCREEN_DISTANCE + ORBIT_CENTER + this.orbitRadius*Math.cos(this.getAngle(t)));
+    const vector = SCREEN_DISTANCE * this.orbitRadius*Math.sin(this.getAngle(t)) /(SCREEN_DISTANCE + ORBIT_CENTER + this.orbitRadius*Math.cos(this.getAngle(t)));
+    this.opticalX = SCREEN_WIDTH/2 + Math.cos(this.diskAngle) * vector;
+    this.opticalY = SCREEN_HEIGHT/2 + Math.sin(this.diskAngle) * vector;
     this.threeDoffset = (EYE_DISTANCE/2) * (Math.cos(this.getAngle(t)) * this.orbitRadius + ORBIT_CENTER) / (Math.cos(this.getAngle(t)) * this.orbitRadius + SCREEN_DISTANCE + ORBIT_CENTER);
   }
 
-  getAngle(t: number): number {
-    return t * 0.002*Math.PI + this.startingAngle;
+  private getAngle(t: number): number {
+    return this.speed * t * 0.002*Math.PI + this.startingAngle;
   }
 }
